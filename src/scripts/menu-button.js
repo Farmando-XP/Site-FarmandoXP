7bf933d086fe9b1a4368c1d79c52acffbb7b3c80
FreitasLeo0102 <freitasleo535@gmail.com>
2026-09-15 15:22:46 -0300

    Delete src directory

--- src/scripts/menu-button.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    let dropdown = document.querySelector('.dropdown-content');
-    dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
-});

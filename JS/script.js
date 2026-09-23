ac8a75295ec2b77af8e769cdb36c2d697d8ca38c
FreitasLeo0102 <freitasleo535@gmail.com>
2026-09-23 20:41:51 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -59,3 +59,59 @@ botoesServico.forEach(function (botao) {
     });
 
 });
+
+const carrossel = document.querySelector(".projetos-carrossel");
+
+if (carrossel) {
+    const slides = Array.from(carrossel.querySelectorAll(".carrossel-slide"));
+    const indicadores = Array.from(carrossel.querySelectorAll(".indicador"));
+    const anterior = carrossel.querySelector(".anterior");
+    const proximo = carrossel.querySelector(".proximo");
+    let slideAtual = 0;
+    let intervalo;
+
+    function mostrarSlide(indice) {
+        slideAtual = (indice + slides.length) % slides.length;
+
+        slides.forEach(function (slide, index) {
+            slide.classList.toggle("ativo", index === slideAtual);
+        });
+
+        indicadores.forEach(function (indicador, index) {
+            const estaAtivo = index === slideAtual;
+            indicador.classList.toggle("ativo", estaAtivo);
+            indicador.setAttribute("aria-selected", estaAtivo);
+        });
+    }
+
+    function iniciarAutoplay() {
+        clearInterval(intervalo);
+        intervalo = setInterval(function () {
+            mostrarSlide(slideAtual + 1);
+        }, 5000);
+    }
+
+    anterior.addEventListener("click", function () {
+        mostrarSlide(slideAtual - 1);
+        iniciarAutoplay();
+    });
+
+    proximo.addEventListener("click", function () {
+        mostrarSlide(slideAtual + 1);
+        iniciarAutoplay();
+    });
+
+    indicadores.forEach(function (indicador, index) {
+        indicador.addEventListener("click", function () {
+            mostrarSlide(index);
+            iniciarAutoplay();
+        });
+    });
+
+    carrossel.addEventListener("mouseenter", function () {
+        clearInterval(intervalo);
+    });
+
+    carrossel.addEventListener("mouseleave", iniciarAutoplay);
+    iniciarAutoplay();
+}
